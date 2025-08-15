1d60a14b7c81ee53f77fab5726d20985186abef1
Nitin-co <nitinsemailbox@gmail.com>
2025-08-16 02:48:14 +0530

    Updated MessageBubble.tsx

--- src/components/chat/MessageBubble.tsx
+++ src/components/chat/MessageBubble.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Message } from '../../types'
+import { Message } from '/home/project/src/types'
 import { Bot, User } from 'lucide-react'
 import clsx from 'clsx'
 
