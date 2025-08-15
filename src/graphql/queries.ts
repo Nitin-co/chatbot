5c6bc9c5325a14c70824fb09a54cd68d9c37ae6c
Nitin-co <nitinsemailbox@gmail.com>
2025-08-15 23:22:12 +0530

    Updated queries.ts

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,18 +1,22 @@
 import { gql } from '@apollo/client'
 
+// Get all chats (latest messages)
 export const GET_CHATS = gql`
   query GetChats {
     chats(order_by: { created_at: desc }) {
       id
       created_at
       messages(order_by: { created_at: asc }, limit: 1) {
+        id
         text
         sender
+        created_at
       }
     }
   }
 `
 
+// Get messages for a specific chat
 export const GET_CHAT_MESSAGES = gql`
   query GetChatMessages($chat_id: uuid!) {
     messages(
@@ -27,19 +31,23 @@ export const GET_CHAT_MESSAGES = gql`
   }
 `
 
+// Subscription for all chats
 export const SUBSCRIBE_TO_CHATS = gql`
   subscription SubscribeToChats {
     chats(order_by: { created_at: desc }) {
       id
       created_at
       messages(order_by: { created_at: asc }, limit: 1) {
+        id
         text
         sender
+        created_at
       }
     }
   }
 `
 
+// Subscription for messages of a chat
 export const SUBSCRIBE_TO_MESSAGES = gql`
   subscription SubscribeToMessages($chat_id: uuid!) {
     messages(
@@ -49,16 +57,6 @@ export const SUBSCRIBE_TO_MESSAGES = gql`
       id
       text
       sender
-      created_at
-    }
-  }
-`
-
-// New: mutation to create a default chat if none exist
-export const CREATE_DEFAULT_CHAT = gql`
-  mutation CreateDefaultChat {
-    insert_chats_one(object: {}) {
-      id
       created_at
     }
   }
