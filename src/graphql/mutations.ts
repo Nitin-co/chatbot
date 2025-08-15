485e14b9cd9cd21c0b638992a2b1ddbe07a11738
Nitin-co <nitinsemailbox@gmail.com>
2025-08-15 22:45:12 +0530

    Updated mutations.ts

--- src/graphql/mutations.ts
+++ src/graphql/mutations.ts
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client'
 
-// Create a new chat
+// Mutation to insert a new chat
 export const CREATE_CHAT = gql`
   mutation CreateChat {
     insert_chats_one(object: {}) {
@@ -10,14 +10,16 @@ export const CREATE_CHAT = gql`
   }
 `
 
-// Insert a message directly (optional, mostly for testing)
+// Mutation to insert a message (user or bot)
 export const INSERT_MESSAGE = gql`
   mutation InsertMessage($chat_id: uuid!, $text: String!, $sender: String!) {
-    insert_messages_one(object: {
-      chat_id: $chat_id
-      text: $text
-      sender: $sender
-    }) {
+    insert_messages_one(
+      object: {
+        chat_id: $chat_id
+        text: $text
+        sender: $sender
+      }
+    ) {
       id
       text
       sender
@@ -26,9 +28,10 @@ export const INSERT_MESSAGE = gql`
   }
 `
 
-export const SEND_MESSAGE = gql`
-  mutation SendMessage($chat_id: uuid!, $text: String!) {
-    sendMessage(input: { chat_id: $chat_id, text: $text }) {
+// Mutation corresponding to your Hasura action sendMessage
+export const SEND_MESSAGE_ACTION = gql`
+  mutation SendMessage($chat_id: uuid!, $message: String!) {
+    sendMessage(input: { chat_id: $chat_id, text: $message }) {
       id
       chat_id
       sender
