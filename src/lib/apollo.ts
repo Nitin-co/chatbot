84ee114b60aa228c53603eca880dcf2955552a30
Nitin-co <nitinsemailbox@gmail.com>
2025-08-17 02:57:35 +0530

    Updated apollo.ts

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,57 +1,43 @@
 // src/lib/apollo.ts
-import { ApolloClient, InMemoryCache, createHttpLink, split } from '@apollo/client'
-import { setContext } from '@apollo/client/link/context'
-import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
-import { getMainDefinition } from '@apollo/client/utilities'
-import { createClient } from 'graphql-ws'
-import { nhost } from './nhost'
+import { ApolloClient, InMemoryCache, HttpLink, split } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 
-// HTTP link
-const httpLink = createHttpLink({
-  uri: nhost.graphql.getUrl(), // Nhost GraphQL endpoint
-})
+// HTTP for queries/mutations
+const httpLink = new HttpLink({
+  uri: import.meta.env.VITE_HASURA_GRAPHQL_URL,
+  headers: {
+    "x-hasura-admin-secret": import.meta.env.VITE_HASURA_ADMIN_SECRET,
+  },
+});
 
-// Auth link: attach JWT token
-const authLink = setContext(async (_, { headers }) => {
-  const token = await nhost.auth.getAccessToken()
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  }
-})
-
-// WebSocket link for subscriptions
+// WS for subscriptions
 const wsLink = new GraphQLWsLink(
   createClient({
-    url: nhost.graphql.getUrl().replace(/^http/, 'ws'), // ws:// or wss://
-    connectionParams: async () => {
-      const token = await nhost.auth.getAccessToken()
-      return {
-        headers: {
-          Authorization: token ? `Bearer ${token}` : '',
-        },
-      }
+    url: import.meta.env.VITE_HASURA_WS_URL!,
+    connectionParams: {
+      headers: {
+        "x-hasura-admin-secret": import.meta.env.VITE_HASURA_ADMIN_SECRET,
+      },
     },
   })
-)
+);
 
-// Split link: use WS for subscriptions, HTTP for queries/mutations
+// Split based on operation type
 const splitLink = split(
   ({ query }) => {
-    const definition = getMainDefinition(query)
+    const definition = getMainDefinition(query);
     return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
-    )
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
   },
   wsLink,
-  authLink.concat(httpLink)
-)
+  httpLink
+);
 
-// Apollo client
 export const apolloClient = new ApolloClient({
   link: splitLink,
   cache: new InMemoryCache(),
-})
+});
