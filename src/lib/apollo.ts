4f6aac2de1409d9285ea249e9877f46d05ba4813
Nitin-co <nitinsemailbox@gmail.com>
2025-08-16 17:10:23 +0530

    Updated apollo.ts

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,4 +1,4 @@
-// apollo.ts
+// src/lib/apollo.ts
 import { ApolloClient, InMemoryCache, createHttpLink, split } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
@@ -8,32 +8,32 @@ import { nhost } from './nhost'
 
 // HTTP link
 const httpLink = createHttpLink({
-  uri: nhost.graphql.getUrl()
+  uri: nhost.graphql.getUrl(), // Nhost GraphQL endpoint
 })
 
-// Auth link (async token fetch)
+// Auth link: attach JWT token
 const authLink = setContext(async (_, { headers }) => {
   const token = await nhost.auth.getAccessToken()
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : ''
-    }
+      authorization: token ? `Bearer ${token}` : '',
+    },
   }
 })
 
 // WebSocket link for subscriptions
 const wsLink = new GraphQLWsLink(
   createClient({
-    url: nhost.graphql.getUrl().replace(/^http/, 'ws'),
+    url: nhost.graphql.getUrl().replace(/^http/, 'ws'), // ws:// or wss://
     connectionParams: async () => {
       const token = await nhost.auth.getAccessToken()
       return {
         headers: {
-          Authorization: token ? `Bearer ${token}` : ''
-        }
+          Authorization: token ? `Bearer ${token}` : '',
+        },
       }
-    }
+    },
   })
 )
 
@@ -53,5 +53,5 @@ const splitLink = split(
 // Apollo client
 export const apolloClient = new ApolloClient({
   link: splitLink,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 })
