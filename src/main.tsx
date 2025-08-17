25e7501bde918259dcb97b53484ac27facb8bed5
Nitin-co <nitinsemailbox@gmail.com>
2025-08-17 12:31:55 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,19 @@
-// main.tsx
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { NhostProvider } from "@nhost/react";
+import { NhostApolloProvider } from "@nhost/react-apollo";
+import { nhost } from "./lib/nhost";
+import App from "./App";
 
-// Import Nhost and Apollo providers
-import { NhostProvider } from '@nhost/react'
-import { NhostApolloProvider } from '@nhost/react-apollo'
-import { nhost } from './lib/nhost.ts'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <NhostProvider nhost={nhost}>
       <NhostApolloProvider nhost={nhost}>
-        <App />
+        <Router>
+          <App />
+        </Router>
       </NhostApolloProvider>
     </NhostProvider>
-  </StrictMode>
-)
+  </React.StrictMode>
+);
