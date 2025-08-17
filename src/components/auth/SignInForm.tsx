eee192b8e2a86c8c3e44737d89479094a96b4ffc
Nitin-co <nitinsemailbox@gmail.com>
2025-08-17 16:16:16 +0530

    Updated SignInForm.tsx

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -30,7 +30,7 @@ export default function SignInForm({ onToggleMode }: SignInFormProps) {
     await signInEmailPassword(email, password)
 
     // Refresh Apollo subscriptions after login
-    import('./apollo').then(({ wsClient }) => {
+    import('/home/project/src/lib/apollo.ts').then(({ wsClient }) => {
       if (wsClient) {
         wsClient.dispose()
       }
