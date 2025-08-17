19c7048955c3521849e21a3fccd6a7b024352dec
Nitin-co <nitinsemailbox@gmail.com>
2025-08-17 21:19:13 +0530

    Updated apollo.ts

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -2,42 +2,10 @@ import { ApolloClient, InMemoryCache, createHttpLink, split } from '@apollo/clie
 import { setContext } from '@apollo/client/link/context'
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
 import { getMainDefinition } from '@apollo/client/utilities'
-import { createClient, Client } from 'graphql-ws'
+import { createClient } from 'graphql-ws'
 import { nhost } from './nhost'
 
-let wsClient: Client | null = null
-
-const createWsClient = () => {
-  return createClient({
-    url: import.meta.env.VITE_HASURA_WS_URL!,
-    lazy: true,
-    retryAttempts: Infinity,
-    connectionParams: async () => {
-      const token = await nhost.auth.getAccessToken()
-      if (!token) {
-        console.warn('[WS] No token available, cannot authenticate WS connection')
-        return {}
-      }
-      return { headers: { Authorization: `Bearer ${token}` } }
-    },
-    on: {
-      connected: () => console.log('[WS] Connected'),
-      closed: () => {
-        console.log('[WS] Connection closed, will recreate...')
-        wsClient = null
-      },
-      error: (err) => console.error('[WS] Connection error:', err),
-    },
-  })
-}
-
-const getWsClient = () => {
-  if (!wsClient) wsClient = createWsClient()
-  return wsClient
-}
-
-const wsLink = new GraphQLWsLink(getWsClient())
-
+// HTTP link with auth
 const httpLink = createHttpLink({
   uri: import.meta.env.VITE_HASURA_GRAPHQL_URL!,
 })
@@ -49,10 +17,29 @@ const authLink = setContext(async (_, { headers }) => {
   }
 })
 
+// WebSocket link with token injected dynamically
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: import.meta.env.VITE_HASURA_WS_URL!,
+    lazy: true,
+    retryAttempts: Infinity,
+    connectionParams: async () => {
+      const token = await nhost.auth.getAccessToken()
+      return token ? { headers: { Authorization: `Bearer ${token}` } } : {}
+    },
+    on: {
+      connected: () => console.log('[WS] Connected'),
+      closed: () => console.log('[WS] Closed'),
+      error: (err) => console.error('[WS] Error', err),
+    },
+  })
+)
+
+// Split queries and subscriptions
 const splitLink = split(
   ({ query }) => {
-    const definition = getMainDefinition(query)
-    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
+    const def = getMainDefinition(query)
+    return def.kind === 'OperationDefinition' && def.operation === 'subscription'
   },
   wsLink,
   authLink.concat(httpLink)
@@ -61,16 +48,11 @@ const splitLink = split(
 export const apolloClient = new ApolloClient({
   link: splitLink,
   cache: new InMemoryCache(),
-  defaultOptions: {
-    watchQuery: { errorPolicy: 'all' },
-    query: { errorPolicy: 'all' },
-  },
+  defaultOptions: { watchQuery: { errorPolicy: 'all' }, query: { errorPolicy: 'all' } },
 })
 
+// Recreate WS client on auth change
 nhost.auth.onAuthStateChanged(() => {
-  if (wsClient) {
-    console.log('[WS] Auth changed, reconnecting...')
-    wsClient.dispose()
-    wsClient = null
-  }
+  console.log('[WS] Auth changed, reloading subscriptions...')
+  wsLink.client.dispose()
 })
