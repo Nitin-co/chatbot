6fc1d9e93a4609478fbdcc739c8bdb8067e25500
Nitin-co <nitinsemailbox@gmail.com>
2025-08-17 22:46:12 +0530

    Updated apollo.ts

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,25 +1,31 @@
+// apollo.ts
 import { ApolloClient, InMemoryCache, createHttpLink, split } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
 import { getMainDefinition } from '@apollo/client/utilities'
 import { createClient, Client } from 'graphql-ws'
-import { nhost } from './nhost'
+import { nhost } from '/home/project/src/lib/nhost'
 
 let wsClient: Client | null = null
 let currentToken: string | null = null
 
-const createWsClient = (token: string) => {
+const createWsClient = () => {
+  if (!currentToken) {
+    console.warn('[WS] No token yet, WS client cannot be created')
+    return null
+  }
+
   return createClient({
     url: import.meta.env.VITE_HASURA_WS_URL!,
     lazy: true,
     retryAttempts: Infinity,
     connectionParams: {
-      headers: { Authorization: `Bearer ${token}` },
+      headers: { Authorization: `Bearer ${currentToken}` },
     },
     on: {
       connected: () => console.log('[WS] Connected'),
       closed: () => {
-        console.log('[WS] Connection closed, will recreate...')
+        console.log('[WS] WS closed, will recreate on next subscription')
         wsClient = null
       },
       error: (err) => console.error('[WS] Connection error:', err),
@@ -28,10 +34,11 @@ const createWsClient = (token: string) => {
 }
 
 const getWsClient = () => {
-  if (!wsClient && currentToken) wsClient = createWsClient(currentToken)
+  if (!wsClient && currentToken) wsClient = createWsClient()
   return wsClient
 }
 
+// GraphQL links
 const wsLink = new GraphQLWsLink(getWsClient()!)
 
 const httpLink = createHttpLink({
@@ -64,12 +71,12 @@ export const apolloClient = new ApolloClient({
   },
 })
 
-// ⚡ Recreate WS client whenever token changes
+// ⚡ Recreate WS client when auth changes
 nhost.auth.onAuthStateChanged(async () => {
   const token = await nhost.auth.getAccessToken()
   currentToken = token
   if (wsClient) {
-    console.log('[WS] Auth changed, reconnecting WS...')
+    console.log('[WS] Auth changed, disposing old WS client...')
     wsClient.dispose()
     wsClient = null
   }
