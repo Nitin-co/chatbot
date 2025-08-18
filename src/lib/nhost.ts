8211925c657c21aac4c6b0ff7f061a147a7acae5
Nitin-co <nitinsemailbox@gmail.com>
2025-08-18 11:04:07 +0530

    Updated nhost.ts

--- src/lib/nhost.ts
+++ src/lib/nhost.ts
@@ -3,4 +3,9 @@ import { NhostClient } from '@nhost/nhost-js'
 export const nhost = new NhostClient({
   subdomain: import.meta.env.VITE_NHOST_SUBDOMAIN || 'rvmtvbxomszjibeiocvu',
   region: import.meta.env.VITE_NHOST_REGION || 'eu-central-1',
-})
+  clientUrl: window.location.origin,
+  clientStorageType: 'localStorage',
+  autoSignIn: true,
+  autoRefreshToken: true
+}
+)
